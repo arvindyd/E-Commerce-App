6af9df80879e3a5cdc69f702bd722179ea587853
Arvind Yadav <arvind9f@gmail.com>
2022-11-21 14:24:26 +0545

    This is my first commit

---
+++ app/src/main/java/com/arvind/e_commmerce/activities/PaymentActivity.java
@@ -0,0 +1,34 @@
+package com.arvind.e_commmerce.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.arvind.e_commmerce.databinding.ActivityPaymentBinding;
+import com.arvind.e_commmerce.utils.Constants;
+
+
+public class PaymentActivity extends AppCompatActivity {
+
+    ActivityPaymentBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityPaymentBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        String orderCode = getIntent().getStringExtra("orderCode");
+
+        binding.webview.setMixedContentAllowed(true);
+        binding.webview.loadUrl(Constants.PAYMENT_URL + orderCode);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
+    }
+}
